336dec108981019a6f730b160ec42bd7ce66853c
thecreativecoder07 <134698213+thecreativecoder07@users.noreply.github.com>
2023-09-04 07:50:12 -0700

    Add files via upload
    updating all javascripts

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // open and close navbar
 function openNav()
 {
-    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("mySidenav").style.width = "300px";
 
 }
 function closeNav()
@@ -80,3 +80,67 @@ function validateForm()
 
 
 }
+
+// scroll to top of screen js
+const scrolltop = document.getElementById('scrollToTopBtn');
+window.addEventListener('scroll', ()=>
+{
+    if(window.scrollY > 300)
+    {
+        scrolltop.style.display = "block";
+    }
+    else{
+        scrolltop.style.display = "none";
+
+    }
+});
+
+scrolltop.addEventListener('click', ()=>
+{
+    window.scrollTo({
+        top: 0,
+        behaviour: "smooth"
+    });
+});
+
+// reveal elements on scroll
+const observer = new IntersectionObserver((entries)=>
+{
+    entries.forEach(entry => {
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add('active');
+        }
+        else{
+            entry.target.classList.remove('active');
+        }
+    });
+});
+
+const hiddenELements = document.querySelectorAll('.hidden');
+hiddenELements.forEach((element)=> observer.observe(element)) ;
+
+
+// feedback carousel
+
+$(".owl-carousel").owlCarousel({
+    items: 4, // Number of items to display
+    loop: true, // Enable loop
+    margin: 10, // Space between items
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1 // Number of items to display at different screen widths
+        },
+        576: {
+            items: 2
+        },
+        768: {
+            items: 3
+        },
+        992: {
+            items: 4
+        }
+    }
+});
+
